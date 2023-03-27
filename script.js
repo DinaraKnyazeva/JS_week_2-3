4a37a6b0073c5089809e4bfcaa722999e0eb7d21
DinaraKnyazeva <Dinarka470@mail.ru>
2023-03-27 20:07:25 +0400

    comleted task2

--- script.js
+++ script.js
@@ -5,6 +5,7 @@
 //     Шаг 4: Год является високосным (366 дней).
 //     Шаг 5: Год не високосный (у него 365 дней).
 // примечания: как в экселе функция "=если" if (если) то (else)
+// вариант №1:
 const positiveResult = "Год является високосным (366 дней)";
 const negativeResult = "Год не високосный (у него 365 дней)";
 
@@ -23,4 +24,25 @@ function calculationDays(year) {
     return negativeResult;
   }
 }
-console.log(calculationDays(400));
+// console.log(calculationDays(400));
+
+// вариант №2 (от кота):
+// Синтаксис:
+// let result = условие ? значение1 : значение2;
+// Сначала вычисляется условие: если оно истинно, тогда возвращается значение1, в противном случае – значение2.
+// Например:
+// let accessAllowed = (age > 18) ? true : false;
+
+function calculationDays(year) {
+  const isLeapYear = Number.isInteger(year / 4)
+    ? Number.isInteger(year / 100)
+      ? Number.isInteger(year / 400)
+        ? true
+        : false
+      : true
+    : false;
+  return isLeapYear
+    ? console.log("Год является високосным (366 дней)")
+    : console.log("Год не високосный (у него 365 дней)");
+}
+calculationDays(2023);
